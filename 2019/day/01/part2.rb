bde0fd87e0ef0a958c33975c294445b33da26b5a
Raph Estrada <raph@intercom.io>
2019-12-02 09:02:04 +0000

    2019 day 01 part2

--- 2019/day/01/part2.rb
+++ 2019/day/01/part2.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
 
 def fuel(mass)
-  (mass / 3).to_i - 2
+  f = (mass / 3).to_i - 2
+  return 0 unless f > 0
+  f + fuel(f)
 end
 
 puts File.readlines("./input.txt").map{ |l| fuel(l.to_f) }.reduce(:+)
