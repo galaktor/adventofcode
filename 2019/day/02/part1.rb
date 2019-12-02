8b9fa6a583dd92d70719265e65faf7d40c8b1ea4
Raph Estrada <raph@intercom.io>
2019-12-02 19:04:44 +0000

    2019 day 02 part1

---
+++ 2019/day/02/part1.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+@input = File.read(ARGV[0]).split(",").map(&:to_i)
+
+def add(p1, p2)
+  @input[p1] + @input[p2]
+end
+
+def mult(p1, p2)
+  @input[p1] * @input[p2]
+end
+
+def halt
+  puts @input.join(",")
+  exit 0
+end
+
+def process(op, p1, p2, dest)
+  case op
+  when 1
+    @input[dest] = add(p1, p2)
+  when 2
+    @input[dest] = mult(p1, p2)
+  when 99
+    halt
+  else
+    raise "unknown operation"
+  end
+end
+
+
+i = 0
+
+@input[1] = 12
+@input[2] = 2
+
+while i < @input.length
+  process(@input[i], @input[i+1], @input[i+2], @input[i+3])
+  i += 4
+end
