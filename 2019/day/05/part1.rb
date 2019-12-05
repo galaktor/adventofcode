cdd65db502efd6f43997b05dca3cd7e0e2f7d444
Raph Estrada <raph@intercom.io>
2019-12-05 08:48:03 +0000

    day 05 basic parameter modes

--- 2019/day/05/part1.rb
+++ 2019/day/05/part1.rb
@@ -1,39 +1,54 @@
 #!/usr/bin/env ruby
 
 require 'readline'
+require 'pry'
 
 @input = File.read(ARGV[0]).split(",").map(&:to_i)
 
 def add(p1, p2)
-  @input[p1] + @input[p2]
+  p1 + p2
 end
 
 def mult(p1, p2)
-  @input[p1] * @input[p2]
+  p1 * p2
 end
 
 def read
   Readline::readline("> ", false).to_i
 end
 
+def print(p1)
+  puts p1
+end
+
+def param(p, mode)
+  case mode
+  when 1
+    p
+  else
+    @input[p]
+  end
+end
+
 def halt
-  puts @input.first
   exit 0
 end
 
 def process(op, p1, p2, dest)
+  pm = op.digits.drop(2)
+  op = op.digits.take(2).reverse.join.to_i
   case op
   when 1
-    @input[dest] = add(p1, p2)
+    @input[dest] = add(param(p1, pm[0]), param(p2, pm[1]))
     return 4
   when 2
-    @input[dest] = mult(p1, p2)
+    @input[dest] = mult(param(p1, pm[0]), param(p2, pm[1]))
     return 4
   when 3
     @input[dest] = read
     return 2
   when 4
-    puts @input[p1]
+    print(param(p1, pm[0]))
     return 2
   when 99
     halt
