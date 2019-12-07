ba1950d86353f30327349b452bbdb47db188b255
Raph Estrada <raph@intercom.io>
2019-12-07 14:41:52 +0000

    amp script for day 07 splits the string

--- 2019/day/07/amp.sh
+++ 2019/day/07/amp.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
 INTCODE="input-test.txt"
+PHASES=$1
 
-echo -e "$1\n0"                     | ./compute.rb $INTCODE | tail -n1 | \
-awk -v p="$2" '{print p "\n" $0}'   | ./compute.rb $INTCODE | tail -n1 | \
-awk -v p="$3" '{print p "\n" $0}'   | ./compute.rb $INTCODE | tail -n1 | \
-awk -v p="$4" '{print p "\n" $0}'   | ./compute.rb $INTCODE | tail -n1 | \
-awk -v p="$5" '{print p "\n" $0}'   | ./compute.rb $INTCODE | tail -n1
+P1="${PHASES:0:1}"
+P2="${PHASES:1:1}"
+P3="${PHASES:2:1}"
+P4="${PHASES:3:1}"
+P5="${PHASES:4:1}"
+
+echo -e  "$P1\n0"                  | ./compute.rb $INTCODE | tail -n1 | \
+awk -v p="$P2" '{print p "\n" $0}' | ./compute.rb $INTCODE | tail -n1 | \
+awk -v p="$P3" '{print p "\n" $0}' | ./compute.rb $INTCODE | tail -n1 | \
+awk -v p="$P4" '{print p "\n" $0}' | ./compute.rb $INTCODE | tail -n1 | \
+awk -v p="$P5" '{print p "\n" $0}' | ./compute.rb $INTCODE | tail -n1
