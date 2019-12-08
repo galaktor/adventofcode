75b82ee719c06456b4f59d27778188d2e24d3295
Raph Estrada <raph@intercom.io>
2019-12-08 11:49:22 +0000

    day 07 part2 amp.sh buffering issues fixed

--- 2019/day/07/amp.sh
+++ 2019/day/07/amp.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-INTCODE="input.txt"
+INTCODE="input-test.txt"
+
 PHASES=$1
 
 P1="${PHASES:0:1}"
@@ -9,8 +10,32 @@ P3="${PHASES:2:1}"
 P4="${PHASES:3:1}"
 P5="${PHASES:4:1}"
 
-echo -e  "$P1\n0"                  | ./compute.rb $INTCODE | tail -n1 | \
-awk -v p="$P2" '{print p "\n" $0}' | ./compute.rb $INTCODE | tail -n1 | \
-awk -v p="$P3" '{print p "\n" $0}' | ./compute.rb $INTCODE | tail -n1 | \
-awk -v p="$P4" '{print p "\n" $0}' | ./compute.rb $INTCODE | tail -n1 | \
-awk -v p="$P5" '{print p "\n" $0}' | ./compute.rb $INTCODE | tail -n1
+echo "deleting fifos"
+rm *.in
+
+echo "making fifos"
+mkfifo amp1.in
+mkfifo amp2.in
+mkfifo amp3.in
+mkfifo amp4.in
+mkfifo amp5.in
+
+echo "starting amps"
+while read line <amp1.in; do echo $line; done | ./compute.rb $INTCODE | grep --line-buffered -v ">" | tee amp2.in &
+while read line <amp2.in; do echo $line; done | ./compute.rb $INTCODE | grep --line-buffered -v ">" | tee amp3.in &
+while read line <amp3.in; do echo $line; done | ./compute.rb $INTCODE | grep --line-buffered -v ">" | tee amp4.in &
+while read line <amp4.in; do echo $line; done | ./compute.rb $INTCODE | grep --line-buffered -v ">" | tee amp5.in &
+while read line <amp5.in; do echo $line; done | ./compute.rb $INTCODE | grep --line-buffered -v ">" | tee amp1.in &
+
+echo "configuring amps"
+echo $P1 >> amp1.in
+echo $P2 >> amp2.in
+echo $P3 >> amp3.in
+echo $P4 >> amp4.in
+echo $P5 >> amp5.in
+
+echo "starting pipeline"
+echo 0 >> amp1.in
+
+echo "waiting"
+wait
