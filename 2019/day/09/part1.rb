01ce3756e4317d4eefdd29d897eb76c22879fcb4
Raph Estrada <raph@intercom.io>
2019-12-09 11:52:27 +0000

    day09 part1 working

--- 2019/day/09/part1.rb
+++ 2019/day/09/part1.rb
@@ -5,8 +5,7 @@ require 'readline'
 STDIN.sync = true
 STDOUT.sync = true
 
-@input = File.read(ARGV[0]).split(",").map(&:to_i)
-@input = @input + Array.new(100*@input.length){ 0 }
+@input = File.read(ARGV[0]).split(",").map(&:to_i) + Array.new(10000){ 0 }
 
 def add(p1, p2)
   p1 + p2
@@ -35,22 +34,33 @@ def param(p, mode)
   end
 end
 
+def store(dest, mode, val)
+  case mode
+  when 1
+    raise "illegal write"
+  when 2
+    @input[dest+@base] = val
+  else
+    @input[dest] = val
+  end
+end
+
 def halt
   exit 0
 end
 
-def process(ptr, op, p1, p2, dest)
-  pm = op.digits.drop(2)
+def process(ptr, op, p1, p2, p3)
+  pm = op.digits.drop(2) + Array.new(4){ 0 }
   op = op.digits.take(2).reverse.join.to_i
   case op
   when 1
-    @input[dest] = add(param(p1, pm[0]), param(p2, pm[1]))
+    store(p3, pm[2], add(param(p1, pm[0]), param(p2, pm[1])))
     return ptr+4
   when 2
-    @input[dest] = mult(param(p1, pm[0]), param(p2, pm[1]))
+    store(p3, pm[2], mult(param(p1, pm[0]), param(p2, pm[1])))
     return ptr+4
   when 3
-    @input[p1] = read
+    store(p1, pm[0], read)
     return ptr+2
   when 4
     print(param(p1, pm[0]))
@@ -60,10 +70,10 @@ def process(ptr, op, p1, p2, dest)
   when 6
     return (param(p1, pm[0]) == 0) ? param(p2, pm[1]) : ptr+3
   when 7
-    @input[dest] = (param(p1, pm[0]) < param(p2, pm[1])) ? 1 : 0
+    store(p3, pm[2], (param(p1, pm[0]) < param(p2, pm[1])) ? 1 : 0)
     return ptr+4
   when 8
-    @input[dest] = (param(p1, pm[0]) == param(p2, pm[1])) ? 1 : 0
+    store(p3, pm[2], (param(p1, pm[0]) == param(p2, pm[1])) ? 1 : 0)
     return ptr+4
   when 9
     @base += param(p1, pm[0])
@@ -74,7 +84,6 @@ def process(ptr, op, p1, p2, dest)
     raise "unknown operation: #{op}"
   end
 end
-
 
 @base = 0
 i = 0
