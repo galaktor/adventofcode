0c17a7b2f0a7b94b8fe887083c6bc06fcabac3bd
Raph Estrada <raph@intercom.io>
2019-12-11 20:51:30 +0000

    much faster mining in golang for 2015 day 04 part2

---
+++ 2015/day/04/part2.go
@@ -0,0 +1,36 @@
+//usr/bin/env go run $0 "$@"; exit
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"strings"
+	"sync"
+)
+
+func main() {
+	matches := make(chan int)
+
+	go func() {
+		for {
+			println(<-matches)
+		}
+	}()
+
+	var wg sync.WaitGroup
+	for i := 940000; i <= 9999999; i += 1 {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			h := md5.New()
+			io.WriteString(h, fmt.Sprintf("yzbqklnj%06d", i))
+			candidate := fmt.Sprintf("%x", h.Sum(nil))
+			if strings.HasPrefix(candidate, "000000") {
+				matches <- i
+			}
+		}(i)
+	}
+
+	wg.Wait()
+}
