be00b090503d994f8ebdbffe6c7d582de2da9d99
Raph Estrada <galaktor@gmx.de>
2020-12-01 09:42:25 +0000

    description and solution for 2020 day1 part2

---
+++ 2020/day/1/part2.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require 'pry'
+
+numbers = File.readlines(ARGV[0]).map{ |l| l.chomp.to_i }
+numbers.each do |n|
+  numbers.each do |m|
+    numbers.each do |o|
+      next unless n+m+o == 2020
+      puts n*m*o
+      exit
+    end
+  end
+end
