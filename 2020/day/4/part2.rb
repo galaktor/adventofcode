eb9c40f4190753a577290725be1448d280f47599
Raph Estrada <galaktor@gmx.de>
2020-12-04 07:59:20 +0000

    2020 day4 part2 solution

---
+++ 2020/day/4/part2.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+passports = [{}]
+File.readlines(ARGV[0]).each do |line|
+  if line.strip == ""
+    passports << {}
+    next
+  end
+  line.strip.split(" ").map{ |d| d.split(":") }.each{ |d| passports.last[d[0]] = d[1] }
+end
+
+valid = passports.select do |p|
+  (p.keys.count == 7 && !p.key?("cid")) || (other = p.keys.count == 8)
+end
+
+valid = valid.select do |p|
+  (p["byr"].to_i.between?(1920,2002)) &&
+    (p["iyr"].to_i.between?(2010,2020)) &&
+    (p["eyr"].to_i.between?(2020,2030)) &&
+    ((p["hgt"].end_with?("cm") && p["hgt"][0..2].to_i.between?(150,193)) || (p["hgt"].end_with?("in") && p["hgt"][0..1].to_i.between?(59,76))) &&
+    (p["hcl"].length == 7 && p["hcl"] =~ /#[0-9a-f]{6}/) &&
+    (["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].include?(p["ecl"])) &&
+    (p["pid"].length == 9 && p["pid"] =~ /[0-9]{9}/)
+end
+
+puts valid.length
